1747ab5c834a1a223fd839d44f23518e904b9cfd
Eduardo Model <edu_model@hotmail.com>
2018-05-15 19:38:10 -0300

    Adição da interface com o Arduino

--- Coffeeino-flavored-with-Telegram.js
+++ Coffeeino-flavored-with-Telegram.js
@@ -7,15 +7,23 @@ const Telegraf = require('telegraf')
 const Markup = require('telegraf/markup')
 //Substitui o token no espaço BOT_TOKEN
 //const BOT_TOKEN = 'seu_token_aqui'
-const BOT_TOKEN = '482598542:AAGc2Dz0MFKo8S6_OtLKS5RCjngdafZ_72w'
+
 const bot = new Telegraf(BOT_TOKEN)
 
-//Status do Arduino
+//Status da cafeteira
 let estaLigado = false
 
 
 //Escopo para o arduino pronto para receber comandos
 arduino.on('ready', () => {
+	//Declaração dos componentes que serão utilizados
+	let rele = new five.Relay(10)
+	let botao = new five.Button(2)
+	let led = new five.Led(13)
+	arduino.repl.inject({
+		led: led
+	})
+
 	bot.start((ctx) => {
 		ctx.reply('Comandos', Markup.keyboard([
 					['/Status'],
@@ -36,13 +44,36 @@ arduino.on('ready', () => {
 	})
 	bot.command('Liga', (ctx) => {
 		estaLigado = true
+		Liga(rele, led)
 		ctx.reply('A cafeteira foi ligada')
 	})
 	bot.command('Desliga', (ctx) => {
 		estaLigado = false
+		Desliga(rele, led)
 		ctx.reply('A cafeteira foi desligada')
 	})
 
+	//Para ligar a cafeteira quando não tiver acesso a internet, usa um simples botão
+	botao.on('press', () =>{
+		estaLigado = !estaLigado
+		if(estaLigado){
+			Liga(rele, led)
+		}
+		else{
+			Desliga(rele, led)
+		}
+	})
 
 	bot.startPolling();
 });
+
+function Liga(rele, led){
+	rele.on()
+	led.on()
+	console.log("Relé ligado")
+}
+function Desliga(rele, led){
+	rele.off()
+	led.off()
+	console.log('Relé desligado')
+}
